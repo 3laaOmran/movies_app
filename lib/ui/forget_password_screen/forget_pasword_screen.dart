3955f774638ac7b0bd35de93d7987feceab2c8fb
iamraoufovic <iammohamedabdelraouf@gmail.com>
2025-01-30 20:57:47 +0200

    forget password screen

---
+++ lib/ui/forget_password_screen/forget_pasword_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import '../../utils/app_colors.dart';
+import '../../utils/app_styles.dart';
+import '../../utils/asset_manager.dart';
+
+class ForgetPasswordScreen extends StatelessWidget {
+  static const String routeName = 'forget_password_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Forget Password",
+          style: AppStyles.regular16Yellow,),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: AppColors.yellowColor,
+          ),
+          onPressed: () {
+
+          },
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(AssetsManager.forgetPasswordImage, fit: BoxFit.fill ),
+          SizedBox(height:height *.2),
+          TextFormField(
+            style: const TextStyle(color: AppColors.whiteColor),
+            decoration: InputDecoration(
+              prefixIcon:
+              const Icon(Icons.email, color: AppColors.whiteColor),
+              hintText: "Email",
+              hintStyle: const TextStyle(color: Colors.white),
+              fillColor: const Color(0xff282A28),
+              filled: true,
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.transparent),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.transparent),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          ),
+          SizedBox(height: height*.2),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.yellowColor,
+              padding: EdgeInsets.symmetric(vertical: height * 0.02),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+            ),
+            onPressed: () {},
+            child: const Text(
+              "Verify Email",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+
+        ],
+      ),
+
+    );
+  }
+  }
