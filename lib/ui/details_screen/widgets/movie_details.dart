2d114d2484dd948d29d9450bb3e60fe7d2e50f36
3laaOmran <115973437+3laaOmran@users.noreply.github.com>
2025-02-14 12:03:52 +0200

    Solve conflict from movie details web view

--- lib/ui/details_screen/widgets/movie_details.dart
+++ lib/ui/details_screen/widgets/movie_details.dart
@@ -13,77 +13,102 @@ class MovieDetails extends StatelessWidget {
   var rating;
   var runTime;
   var likeCount;
-  int year;
+  num year;
   var url;
-  MovieDetails({
-    required this.url,
-    required this.imagePath,
-    required this.likeCount,
-    required this.movieName,
-    required this.rating,
-    required this.runTime,
-    required this.year
-});
+
+  MovieDetails(
+      {required this.url,
+      required this.imagePath,
+      required this.likeCount,
+      required this.movieName,
+      required this.rating,
+      required this.runTime,
+      required this.year});
 
   @override
   Widget build(BuildContext context) {
-    var height=MediaQuery.of(context).size.height;
-    var width=MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
     return SingleChildScrollView(
-      child: Column(children: [
-        Stack(
-          alignment: Alignment.bottomCenter,
-          // alignment:Alignment.center,
-          children: [
-            Opacity(
-              opacity: 0.5,
-              child: CachedNetworkImage(
-                imageUrl:  imagePath,
+      child: Column(
+        children: [
+          Stack(
+            alignment: Alignment.bottomCenter,
+            // alignment:Alignment.center,
+            children: [
+              Opacity(
+                  opacity: 0.5,
+                  child: CachedNetworkImage(
+                    imageUrl: imagePath,
+                  )),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Center(
+                      child: InkWell(
+                          onTap: () {
+                            launch(url);
+                          },
+                          child: Image.asset(AssetsManager.playIcon))),
+                  SizedBox(
+                    height: height * 0.17,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        movieName,
+                        style: AppStyles.bold24White,
+                      ),
+                      Text(
+                        '$year',
+                        style: AppStyles.bold20White,
+                      ),
+                    ],
+                  )
+                ],
               )
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Center(child: InkWell(
-                    onTap: (){
-                      launch(url);
-                    },
-                    child: Image.asset(AssetsManager.playIcon))),
-               SizedBox(height: height*0.17,),
-               Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: [
-                 Text(movieName,style: AppStyles.bold24White,),
-                 Text('$year',style: AppStyles.bold20White,),
-               ],)
-            ],)
-          ],
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: width*0.04,vertical: height*0.01),
-          child: CustomElevatedButton(
-            border: BorderSide(color: AppColors.redColor),
-            buttonText: 'Watch',
-            buttonTextStyle: AppStyles.bold20White,
-            onPressed: (){
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: width * 0.04, vertical: height * 0.01),
+            child: CustomElevatedButton(
+              border: BorderSide(color: AppColors.redColor),
+              buttonText: 'Watch',
+              buttonTextStyle: AppStyles.bold20White,
+              onPressed: () {
                 launch(url);
               },
-            bgColor: AppColors.redColor,
+              bgColor: AppColors.redColor,
+            ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: width*0.04,vertical: height*0.01),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-          Expanded(child: CustomContainerWidget(imagePath:AssetsManager.favoriteIcon , number: likeCount)),
-          SizedBox(width: width*0.02,),
-            Expanded(child: CustomContainerWidget(imagePath:AssetsManager.clockIcon , number: runTime)),
-            SizedBox(width: width*0.02,),
-            Expanded(child: CustomContainerWidget(imagePath:AssetsManager.ratingIcon , number: rating )),
-          ],),
-        )
-      ],
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: width * 0.04, vertical: height * 0.01),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: CustomContainerWidget(
+                        imagePath: AssetsManager.favoriteIcon,
+                        number: likeCount)),
+                SizedBox(
+                  width: width * 0.02,
+                ),
+                Expanded(
+                    child: CustomContainerWidget(
+                        imagePath: AssetsManager.clockIcon, number: runTime)),
+                SizedBox(
+                  width: width * 0.02,
+                ),
+                Expanded(
+                    child: CustomContainerWidget(
+                        imagePath: AssetsManager.ratingIcon, number: rating)),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
